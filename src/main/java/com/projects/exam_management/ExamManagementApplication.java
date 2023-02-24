e586a36cb151c0406185175c846554350b499cbd
mahmoud sayed <mahmoudsayed1332002@gmail.com>
2023-02-24 19:13:43 +0200

    Initialize the project maven files

---
+++ src/main/java/com/projects/exam_management/ExamManagementApplication.java
@@ -0,0 +1,13 @@
+package com.projects.exam_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/exam_management/ExamManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.exam_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
