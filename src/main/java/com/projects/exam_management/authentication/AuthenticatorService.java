f05193fe65c016e2d675035c6acdc63d4812a277
mahmoud sayed <mahmoudsayed1332002@gmail.com>
2023-02-25 20:22:17 +0200

    update data to DoctorDTO for encapsulation

--- src/main/java/com/projects/exam_management/authentication/AuthenticatorService.java
+++ src/main/java/com/projects/exam_management/authentication/AuthenticatorService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projects.exam_management.doctor.Doctor;
-import com.projects.exam_management.doctor.DoctorDTO;
 import com.projects.exam_management.doctor.DoctorService;
 
 @Service
